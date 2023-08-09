c33a9fd570739d73236689f33ee5a6c5c890e89e
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-08-09 12:45:36 +0530

    Update pathsumroot_to_leaf.py

--- CN_Python/Trees/pathsumroot_to_leaf.py
+++ CN_Python/Trees/pathsumroot_to_leaf.py
@@ -1,3 +1,5 @@
+#all paths for a given Binary Tree of type integer and a number K, print out all root-to-leaf paths where the sum of all the node
+#data along the path is equal to K.
 from sys import stdin, setrecursionlimit
 import queue
 
