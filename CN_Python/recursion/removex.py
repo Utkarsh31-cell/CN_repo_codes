dcfbaec1b7d59b62a781b8b69633a913eb557b7c
Utkarsh31-cell <78687661+Utkarsh31-cell@users.noreply.github.com>
2023-08-09 12:46:45 +0530

    Update removex.py

--- CN_Python/recursion/removex.py
+++ CN_Python/recursion/removex.py
@@ -1,1 +1,16 @@
+# Problem: Remove x from string
+def removeX(string,s): 
+    pass
+    if len(string)==0:
+        return s
+    if(string[0]!='x'):
+        s+=string[0]
+
+    return removeX(string[1:],s)
+
+
+# Main
+string = input()
+s=""
+print(removeX(string,s))
 
